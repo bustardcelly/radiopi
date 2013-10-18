0fe7e5f3dd98ea7aeca29e3faf9551d9572efa8d
Todd Anderson <bustardcelly@gmail.com>
2013-10-18 15:03:24 -0400

    trying to resovle instancemethod exception on deriving range value on dial.

--- radiopi/control/dial.py
+++ radiopi/control/dial.py
@@ -1,10 +1,10 @@
 
 class Dial:
   def __init__(self):
-    self.minimum = 0
-    self.maximum = 100
     self.listeners = []
     self.input_value = 0
+    self.minimum_value = 0
+    self.maximum_value = 100
 
   def add_listener(self, listener):
     self.listeners.append(listener)
@@ -14,9 +14,10 @@ class Dial:
       listener(self.input_value)
 
   def range(self, min_value, max_value):
-    self.minimum = min_value
-    self.maximum = max_value
+    self.minimum_value = min_value
+    self.maximum_value = max_value
 
   def set_value(self, percentage):
-    self.input_value = int(self.minimum + round((self.maximum - self.minimum) * percentage))
+    print 'Dial.set_value() :: min: %d, max: %d' % (self.minimum_value, self.maximum_value)
+    self.input_value = int(self.minimum_value + round((self.maximum_value - self.minimum_value) * percentage))
     self.notify_listeners()
