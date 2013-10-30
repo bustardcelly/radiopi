fee79e455a3f3203948781b293f29c8bb7995603
Todd Anderson <bustardcelly@gmail.com>
2013-10-30 09:34:59 -0400

    commands for lcd display communication.

--- radiopi/control/display.py
+++ radiopi/control/display.py
@@ -5,6 +5,9 @@ import radiopi.settings as settings
 from radiopi import prettyprint
 from radiopi import COLORS
 
+CLEAR = "x\0C"
+WRITE = "\xfe\x01"
+
 def split(text):
   return text.rsplit(settings.FILEDATA_DELIMITER)
 
@@ -23,11 +26,12 @@ class LCDDisplay():
   def __init__(self):
     self.context = None
     self.ser = serial.Serial('/dev/ttyAMA0',9600,timeout=0.1)
-    self.ser.write("x\0C")
+    self.ser.write(CLEAR)
 
   def show(self, text):
     if self.context != text:
       lines = split(text)
+      self.ser.write(WRITE)
       for line in split(text):
         prettyprint(COLORS.BLUE, line)
       self.context = text
