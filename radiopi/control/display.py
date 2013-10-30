c975945553cdd86955164fd249fc159389112fe0
Todd Anderson <bustardcelly@gmail.com>
2013-10-30 10:10:07 -0400

    row/column and trim/pad support on lcd display.

--- radiopi/control/display.py
+++ radiopi/control/display.py
@@ -11,6 +11,14 @@ WRITE = "\xfe\x01"
 def split(text):
   return text.rsplit(settings.FILEDATA_DELIMITER)
 
+def trim_pad(text, max_length):
+  text_length = len(text)
+  if text_length < max_length:
+    text.ljust(max_length - text_length)
+  elif text_length > max_length:
+    text = text[:max_length-1]
+  return text
+
 class ConsoleDisplay():
   def __init__(self):
     self.context = None
@@ -23,8 +31,10 @@ class ConsoleDisplay():
       self.context = text
 
 class LCDDisplay():
-  def __init__(self):
+  def __init__(self, columns, rows):
     self.context = None
+    self.columns = columns
+    self.rows = rows
     self.ser = serial.Serial('/dev/ttyAMA0',9600,timeout=0.1)
     self.ser.write(CLEAR)
 
@@ -32,7 +42,8 @@ class LCDDisplay():
     if self.context != text:
       lines = split(text)
       self.ser.write(WRITE)
-      for line in split(text):
+      for line in split(text)[:self.rows]:
+        line = trim_pad(line)
         prettyprint(COLORS.BLUE, line)
       self.context = text
 
