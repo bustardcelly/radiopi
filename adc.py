a921dcf1bc48c7aa2bf1dae555fe429473fe8841
Todd Anderson <bustardcelly@gmail.com>
2014-10-17 11:01:03 -0400

    update to adc script.

--- adc.py
+++ adc.py
@@ -1,7 +1,10 @@
 from radiopi.control.mcp3008 import ADC
+from radiopi.control.mcp3008 import ADC2
+import RPi.GPIO as GPIO
 import time
 
 if __name__ == '__main__':
+  GPIO.setmode(GPIO.BCM)
   adc = ADC()
   adc.open()
   while True:
